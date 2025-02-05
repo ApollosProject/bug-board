c0e0ae461bceea16bf660c281362ead73fecb116
Michael Neeley <micneeley14@gmail.com>
2025-02-04 19:09:29 -0500

    stats

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 from flask import Flask, render_template
 
-from bb import get_priority_issues
+from bb import (
+    get_completed_priority_issues,
+    get_lead_time_data,
+    get_open_priority_issues,
+)
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    issues = get_priority_issues()
-    return render_template("index.html", priority_issues=issues["issues"]["nodes"])
+    open_issues = get_open_priority_issues()
+    completed_issues = get_completed_priority_issues()
+    lead_time_data = get_lead_time_data(completed_issues)
+    print(lead_time_data)
+    return render_template(
+        "index.html",
+        priority_issues=open_issues["issues"]["nodes"],
+        completed_issue_count=len(completed_issues["issues"]["nodes"]),
+        lead_time_data=lead_time_data,
+    )
 
 
 if __name__ == "__main__":
--- bb.py
+++ bb.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 
 from dotenv import load_dotenv
 from gql import Client, gql
@@ -12,11 +13,11 @@ transport = AIOHTTPTransport(url="https://api.linear.app/graphql", headers=heade
 client = Client(transport=transport, fetch_schema_from_transport=True)
 
 
-def get_priority_issues():
+def get_open_priority_issues():
 
     query = gql(
         """
-        query Issues {
+        query PriorityIssues {
           issues(
             filter: {
               labels: { name: { eq: "Bug" } }
@@ -46,3 +47,56 @@ def get_priority_issues():
 
     # Execute the query on the transport
     return client.execute(query)
+
+
+def get_completed_priority_issues():
+
+    query = gql(
+        """
+        query CompletedIssues {
+          issues(
+            filter: {
+              labels: { name: { eq: "Bug" } }
+              project: { name: { eq: "Customer Success" } }
+              priority: { lte: 2 }
+              state: { name: { in: ["Done", "Canceled"] } }
+              completedAt:{lt:"P1M"}
+            }
+            orderBy: updatedAt
+          ) {
+            nodes {
+              id
+              title
+              assignee {
+                name
+              }
+              url
+              labels {
+                nodes {
+                  name
+                }
+              }
+              completedAt
+              createdAt
+            }
+          }
+        }
+        """
+    )
+
+    # Execute the query on the transport
+    return client.execute(query)
+
+
+def get_lead_time_data(issues):
+    lead_times = []
+    for issue in issues["issues"]["nodes"]:
+        completed_at = datetime.strptime(issue["completedAt"], "%Y-%m-%dT%H:%M:%S.%fZ")
+        created_at = datetime.strptime(issue["createdAt"], "%Y-%m-%dT%H:%M:%S.%fZ")
+        lead_time = (completed_at - created_at).days
+        lead_times.append(lead_time)
+    data = {
+        "avg": int(sum(lead_times) / len(lead_times)),
+        "p95": int(sorted(lead_times)[int(len(lead_times) * 0.95)]),
+    }
+    return data
