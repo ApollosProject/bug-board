8075dbdb09bf4a69b8aa57ce6c79f482c2933aa3
Michael Neeley <micneeley14@gmail.com>
2025-03-11 13:47:35 -0400

    show max speed

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def index(days=30):
     completed_bugs = get_completed_issues(5, "Bug", days)
     completed_new_features = get_completed_issues(5, "New Feature", days)
     time_data = get_time_data(completed_priority_bugs)
-    fixes_per_day = len(completed_priority_bugs) / days
+    issues_per_day = len(completed_bugs + completed_new_features) / days
     return render_template(
         "index.html",
         days=days,
@@ -37,7 +37,7 @@ def index(days=30):
         issues_by_reviewer=by_reviewer(completed_bugs + completed_new_features),
         lead_time_data=time_data["lead"],
         queue_time_data=time_data["queue"],
-        fixes_per_day=fixes_per_day,
+        issues_per_day=issues_per_day,
     )
 
 
