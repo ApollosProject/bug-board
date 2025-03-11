dfbbed4a9ec5e67cc79d0e4f2e86f5d06dd5de90
Michael Neeley <micneeley14@gmail.com>
2025-03-11 11:28:57 -0400

    add overdue

--- jobs.py
+++ jobs.py
@@ -6,7 +6,7 @@ import requests
 import schedule
 from dotenv import load_dotenv
 
-from linear import get_completed_issues, get_open_issues, get_time_data
+from linear import get_open_issues
 
 load_dotenv()
 
@@ -26,36 +26,44 @@ def with_retries(func):
 @with_retries
 def post_priority_bugs():
     open_priority_bugs = get_open_issues(2, "Bug")
-    completed_priority_bugs = get_completed_issues(2, "Bug")
-    completed_lead_time_data = get_time_data(completed_priority_bugs)["lead"]
-    unassigned_priority_bugs = [
-        bug for bug in open_priority_bugs if bug["assignee"] is None
-    ]
-    open_issues_over_avg_lead_time = [
+    unassigned = [bug for bug in open_priority_bugs if bug["assignee"] is None]
+    at_risk = [
         bug
         for bug in open_priority_bugs
-        if bug["daysOpen"] > completed_lead_time_data["avg"]
+        if bug["daysOpen"] > 4 and bug["daysOpen"] <= 7
     ]
+    overdue = [bug for bug in open_priority_bugs if bug["daysOpen"] > 7]
 
     markdown = ""
-    if unassigned_priority_bugs:
+    if unassigned:
         markdown += "*Unassigned Priority Bugs*\n\n"
         markdown += "\n".join(
             [
                 f"- <{bug['url']}|{bug['title']}>{' (' + '+' + str(bug['daysOpen']) + 'd'}{', ' + bug['platform'] if bug['platform'] else ''})"
-                for bug in sorted(
-                    unassigned_priority_bugs, key=lambda x: x["daysOpen"], reverse=True
-                )
+                for bug in sorted(unassigned, key=lambda x: x["daysOpen"], reverse=True)
             ]
         )
         markdown += "\n\n"
-    if open_issues_over_avg_lead_time:
+    if at_risk:
         markdown += "*At Risk Issues*\n\n"
         markdown += "\n".join(
             [
                 f"- <{bug['url']}|{bug['title']}>{' (' + '+' + str(bug['daysOpen']) + 'd'}{', ' + bug['assignee']['name'] if bug['assignee'] else ''})"
                 for bug in sorted(
-                    open_issues_over_avg_lead_time,
+                    at_risk,
+                    key=lambda x: x["daysOpen"],
+                    reverse=True,
+                )
+            ]
+        )
+        markdown += "\n\n"
+    if overdue:
+        markdown += "*Overdue Issues*\n\n"
+        markdown += "\n".join(
+            [
+                f"- <{bug['url']}|{bug['title']}>{' (' + '+' + str(bug['daysOpen']) + 'd'}{', ' + bug['assignee']['name'] if bug['assignee'] else ''})"
+                for bug in sorted(
+                    overdue,
                     key=lambda x: x["daysOpen"],
                     reverse=True,
                 )
